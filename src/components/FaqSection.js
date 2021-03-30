a65babc7de0cddc1fc2314756c773a84f61bf997
Adauto <adautofabiano.silva@gmail.com>
2021-03-30 20:29:49 -0300

    Criação do component FAQ

---
+++ src/components/FaqSection.js
@@ -0,0 +1,42 @@
+import react from  'react';
+
+
+const FaqSection = ()=>{
+
+    return (
+        <div className="faq">
+            <h2>Any questions <span>FAQ</span> </h2>
+            <div className="question">
+                <h4>How do I Start?</h4>
+                <div className="answe">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, tempore.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Lorem, ipsum dolor.?</h4>
+                <div className="answe">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, tempore.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Lorem ipsum dolor sit amet consectetur.?</h4>
+                <div className="answe">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, tempore.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Lorem ipsum dolor sit, amet consectetur adipisicing.</h4>
+                <div className="answe">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, tempore.</p>
+                </div>
+            </div>
+        </div>
+    );
+
+}
+
+export default FaqSection;
