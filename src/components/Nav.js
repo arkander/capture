4bd3030ab1d02c6e4e173f719d943eb47d8bd73b
Adauto <adautofabiano.silva@gmail.com>
2021-03-31 19:27:27 -0300

    Criação da pagina contact us

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,23 @@
 import react from  'react';
 import styled from  'styled-components';
+import {Link} from 'react-router-dom';
 
 const Nav = ()=>{
 
     return (
         <StyledNav>
             <h1>
-                <a id="logo" href="#">Capture</a>
+                <Link id="logo" to="/">Capture</Link>
             </h1>
             <ul>
                 <li>
-                    <a href="#">1. About Us</a>
+                    <Link to="/">About Us</Link>
                 </li>
                 <li>
-                    <a href="#">2. Our Work</a>
+                    <Link to="/work">Our Work</Link>
                 </li>
                 <li>
-                    <a href="#">3.Contact Us</a>
+                    <Link to="/contact">Contact Us</Link>
                 </li>
             </ul>
 
